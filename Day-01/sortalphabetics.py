98233af5797dd2e0cffb4ccea76affc9cf9ea140
annamani <annamanik.vit@gmail.com>
2024-02-05 18:16:28 +0100

    sample

--- Day-01/sortalphabetics.py
+++ Day-01/sortalphabetics.py
@@ -1,5 +1,6 @@
 str_1 = input ("Enter a string: ")
 words = [word.lower () for word in str_1.split ()]
+# Print the words in alphabetical order
 words.sort ()
 print("The words sorted in alphabetical order are as follows: ")
 for word in words:
