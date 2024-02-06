ba92ece506b09d64c0a3c3827a8bc4ab28df9735
annamani <annamanik.vit@gmail.com>
2024-02-06 20:01:11 +0100

    sample

--- Day-01/mul_list.py
+++ Day-01/mul_list.py
@@ -1,7 +1,7 @@
-def sum_list(items):
-    sum_numbers = 0
+def mul_list(items):
+    mul_numbers = 0
     for x in items:
         sum_numbers += x
         return sum_numbers
 
-print(sum_list([1, 2, -8]))
+print(mul_list([1, 2, -8]))
