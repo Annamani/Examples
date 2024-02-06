95b7e32fd678a8a81db5d0b6a451fbd314196369
annamani <annamanik.vit@gmail.com>
2024-02-06 19:59:24 +0100

    sample

--- Day-01/sum_list.py
+++ Day-01/sum_list.py
@@ -4,5 +4,4 @@ def sum_list(items):
         sum_numbers += x
         return sum_numbers
 
-# Call the sum_list function with the list [1, 2, -8] as input and print the result
 print(sum_list([1, 2, -8]))
