b400eb1c57125e08ef2af43e5d633736ccf50990
annamani <annamanik.vit@gmail.com>
2024-02-07 17:51:53 +0100

    sample

--- Day-01/mul_list.py
+++ Day-01/mul_list.py
@@ -4,7 +4,7 @@ def mul_list(items):
         total *= x
     return total
 
-# print(mul_list([1, 2, -8]))
+print(mul_list([1, 2, -8]))
 print(mul_list([1, 12, 18]))
 print(mul_list([-11,-11]))
 print(mul_list([0,-11]))
