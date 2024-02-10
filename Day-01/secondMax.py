ba920a5a7bf420b2b5e69879fa9b42704c6c6df1
annamani <annamanik.vit@gmail.com>
2024-02-10 15:30:50 +0100

    sample

--- Day-01/secondMax.py
+++ Day-01/secondMax.py
@@ -3,5 +3,5 @@ n=int(input("Enter number of elements:"))
 for i in range(1,n+1):
     b=int(input("Enter element:"))
     a.append(b)
-a.sort()
+# a.sort()
 # print("Second largest element is:",a[n-2])
