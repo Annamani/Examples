822931bb317b064217df15d7d85c287b9011270a
annamani <annamanik.vit@gmail.com>
2024-02-12 17:50:38 +0100

    examples

--- Day-02/firstLast.py
+++ Day-02/firstLast.py
@@ -1,3 +1,4 @@
-color_list = ["Red","Green","White" ,"Black"]
+# color_list = ["Red","Green","White" ,"Black"]
+color_list=input("Enter some colors: ")
 print("First element: ",color_list[0])
 print("Last element: ",color_list[-1])
