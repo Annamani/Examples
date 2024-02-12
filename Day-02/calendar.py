5f9cf1b378109444802c57b1e42c69fb21e49cc9
annamani <annamanik.vit@gmail.com>
2024-02-12 18:46:27 +0100

    examples

--- Day-02/calendar.py
+++ Day-02/calendar.py
@@ -1,7 +1,7 @@
 import calendar
 # Prompt the user to input the year and month
-year = int(input("Input the year : "))
-month = int(input("Input the month : "))
+year = (input("Input the year : "))
+month = (input("Input the month : "))
 
 # printing with monthdatescalendar
 print(calendar.month(year, month))
