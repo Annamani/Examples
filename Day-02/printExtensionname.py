d9176c03cf736b5dd605ae78be497be1810a9984
annamani <annamanik.vit@gmail.com>
2024-02-12 17:42:16 +0100

    examples

--- Day-02/printExtensionname.py
+++ Day-02/printExtensionname.py
@@ -1,3 +1,4 @@
 filename=input("Enter a file name: ")
 extensionName=filename.split(".")
-print("File Extension is : "+repr(extensionName[-1]))
+# print("File Extension is : "+repr(extensionName[-1]))
+print("File Extension is : ",extensionName[-1])
