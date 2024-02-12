3b16a8722379b440d6c3070cb4998dd42ea234f3
annamani <annamanik.vit@gmail.com>
2024-02-12 17:36:38 +0100

    examples

---
+++ Day-02/covert2ListTuple.py
@@ -0,0 +1,8 @@
+numbers=int(input("Enter some comma-separated numbers: "))
+list = numbers.split(",")
+tuple=tuple(list)
+# Print the list
+print('List : ', list)
+
+# Print the tuple
+print('Tuple : ', tuple)
