9fa2fad72e683d85466c86d27cb080ddcdff2fbe
annamani <annamanik.vit@gmail.com>
2024-02-13 19:04:14 +0100

    examples

--- Day-03/diffdays.py
+++ Day-03/diffdays.py
@@ -1,6 +1,6 @@
 from datetime import date
 
 dob=date(1997, 1, 23)
-current_date=date(2024,2,13)
+current_date=date(2024,5,13)
 age=current_date-dob
-print("Age is : ",age)
+print("Age in days : ",age)
