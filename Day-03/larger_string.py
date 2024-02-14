e102f56f1a3ce83e35b265eb37d289717d7c0a4f
annamani <annamanik.vit@gmail.com>
2024-02-14 18:05:27 +0100

    examples

--- Day-03/larger_string.py
+++ Day-03/larger_string.py
@@ -1,13 +1,7 @@
-# Define a function named "larger_string" that takes two parameters, "text" and "n"
 def larger_string(text, n):
-    # Initialize an empty string variable named "result"
     result = ""
-
-    # Use a for loop to repeat the "text" "n" times and concatenate it to the "result"
     for i in range(n):
         result = result + text
-
-    # Return the final "result" string
     return result
 
 print(larger_string('abc', 2))
