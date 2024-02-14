8ce51550a5c8708a837dda1a353dfd416e3eea3a
annamani <annamanik.vit@gmail.com>
2024-02-14 17:55:31 +0100

    examples

--- Day-03/sum_thrice.py
+++ Day-03/sum_thrice.py
@@ -5,6 +5,4 @@ def sum_thrice(x, y, z):
     return sum
 
 print(sum_thrice(1, 2, 3))
-
-# Call the "sum_thrice" function with the arguments (3, 3, 3) and print the result
 print(sum_thrice(3, 3, 3))
