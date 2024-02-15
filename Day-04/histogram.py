d006e4311622e1a0927129fe7a6d391ae18ce024
annamani <annamanik.vit@gmail.com>
2024-02-15 17:47:53 +0100

    examples

---
+++ Day-04/histogram.py
@@ -0,0 +1,16 @@
+def histogram(numbers):
+    for n in numbers:
+        output = ''  # Initialize an empty string called output.
+        times = n     # Set the times variable to the value of n.
+        
+        # Use a while loop to append '*' to the output string 'times' number of times.
+        while times > 0:
+            output += '*'
+            times = times - 1  # Decrement the times variable.
+        
+        # Print the resulting output string.
+        print(output)
+
+
+
+histogram([2, 3, 6, 5])
