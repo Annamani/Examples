a9166c2aab36bdbf17205ea5aecffad2051a8e8c
annamani <annamanik.vit@gmail.com>
2024-02-15 17:45:41 +0100

    examples

--- Day-04/checkNum.py
+++ Day-04/checkNum.py
@@ -1,8 +1,8 @@
 def check_num(group_data, n):
     for value in group_data:
         if n == value:
-            return True  # If found, return True.
-    return False  # If the loop completes and no match is found, return False.
+            return True  
+    return False  
 
 print(check_num([1, 5, 8, 3], 3)) 
 print(check_num([5, 8, 3], -1))
