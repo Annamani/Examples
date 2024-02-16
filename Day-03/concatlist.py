7fabf699ca8c167f3203d70dca53e7c60cf5cfa6
annamani <annamanik.vit@gmail.com>
2024-02-16 18:42:02 +0100

    examples

--- Day-03/concatlist.py
+++ Day-03/concatlist.py
@@ -2,7 +2,7 @@ def concatenate_list_data(lst):
     result = ''      
     for element in lst:
         result += str(element) 
-    return result  # Return the concatenated string.
+    return result  
 
 # Call the concatenate_list_data function with a list of numbers and print the result.
 print(concatenate_list_data([1, 5, 12, 2]))
