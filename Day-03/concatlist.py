afdfce15254edfaa8c2ac2a0fedba8d13dc33910
annamani <annamanik.vit@gmail.com>
2024-02-16 18:41:14 +0100

    examples

--- Day-03/concatlist.py
+++ Day-03/concatlist.py
@@ -1,6 +1,5 @@
 def concatenate_list_data(lst):
     result = ''      
-    # Iterate through the elements in the list.
     for element in lst:
         result += str(element)  # Convert each element to a string and concatenate it to the result.
 
