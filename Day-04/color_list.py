524d743aad24c734577103e2764f1d5e604d126a
annamani <annamanik.vit@gmail.com>
2024-02-19 13:07:35 +0100

    examples

--- Day-04/color_list.py
+++ Day-04/color_list.py
@@ -1,11 +1,12 @@
 color_list_1 = set(["White", "Black", "Red"])
 color_list_2 = set(["Red", "Green"])
+
 print("Original set elements:")
 print(color_list_1)
 print(color_list_2)
+
 print("\nDifference of color_list_1 and color_list_2:")
 print(color_list_1.difference(color_list_2))
 
-# Calculate and print the difference of color_list_2 and color_list_1.
 print("\nDifference of color_list_2 and color_list_1:")
 print(color_list_2.difference(color_list_1))
