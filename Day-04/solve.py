8e220e7fa8830754ab08bccac88d46c139e47b24
annamani <annamanik.vit@gmail.com>
2024-02-20 21:07:42 +0100

    examples

--- Day-04/solve.py
+++ Day-04/solve.py
@@ -1,4 +1,6 @@
 x, y = 4, 3
-result = x * x + 2 * x * y + y * y
+# result = x * x + 2 * x * y + y * y
+a=x+y
+result=a*a
 # Print the result with a formatted message.
 print("({} + {}) ^ 2) = {}".format(x, y, result))
