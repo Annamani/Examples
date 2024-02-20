ebe61e7aaf78dc61b3a1a3741655193c290495eb
annamani <annamanik.vit@gmail.com>
2024-02-20 21:06:49 +0100

    examples

--- Day-04/solve.py
+++ Day-04/solve.py
@@ -1,5 +1,4 @@
 x, y = 4, 3
-# Calculate the result by squaring the sum of 'x' and 'y'.
 result = x * x + 2 * x * y + y * y
 # Print the result with a formatted message.
 print("({} + {}) ^ 2) = {}".format(x, y, result))
