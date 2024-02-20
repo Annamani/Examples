790285f4a835cbb0e8a1b9db0cee9d09f70b69e0
annamani <annamanik.vit@gmail.com>
2024-02-20 21:04:39 +0100

    examples

--- Day-04/personalDetails.py
+++ Day-04/personalDetails.py
@@ -1,7 +1,6 @@
 def personal_details():
     name, age = "Simon", 19
     address = "Bangalore, Karnataka, India"
-    
     print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
 
 # Call the 'personal_details' function to display the details.
