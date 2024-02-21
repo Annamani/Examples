1497754bafd16a5e00949e6101e68712bb7215c4
annamani <annamanik.vit@gmail.com>
2024-02-21 17:12:37 +0100

    examples

--- Day-05/distance.py
+++ Day-05/distance.py
@@ -2,9 +2,6 @@ import math
 
 p1 = [4, 0]
 p2 = [6, 6]
-
-# Calculate the distance between the two points using the distance formula.
-# The formula computes the Euclidean distance in a 2D space.
 distance = math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
 
 # Print the calculated distance.
