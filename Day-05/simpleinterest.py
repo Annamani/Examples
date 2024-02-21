60a57b1d5f3a12aaa80f45322aae7093a166e56b
annamani <annamanik.vit@gmail.com>
2024-02-21 17:02:41 +0100

    examples

--- Day-05/simpleinterest.py
+++ Day-05/simpleinterest.py
@@ -2,7 +2,6 @@
 amt = 10000
 # Define the annual interest rate as a percentage.
 int = 3.5
-# Define the number of years.
 years = 7
 # FV = P(1 + r/n)^nt.
 future_value = amt * ((1 + (0.01 * int)) ** years)
