f178ca87bd65da5830b4fb717d30026d54876108
annamani <annamanik.vit@gmail.com>
2024-02-22 19:35:16 +0100

    examples

--- Day-05/getusername.py
+++ Day-05/getusername.py
@@ -1,4 +1,3 @@
-# Import the 'getpass' module to access user-related functionality, including getting the current username.
 import getpass
 
 # Use the 'getuser()' function from the 'getpass' module to retrieve the current username.
