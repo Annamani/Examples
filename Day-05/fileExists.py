53e01ce4357ece4862a5ef9a35cbcf1d1ee9d280
annamani <annamanik.vit@gmail.com>
2024-02-22 19:32:23 +0100

    examples

---
+++ Day-05/fileExists.py
@@ -0,0 +1,7 @@
+import os.path
+
+# Check if 'main.txt' is a file and print the result.
+print(os.path.isfile('main.txt'))
+
+# Check if 'main.py' is a file and print the result.
+print(os.path.isfile('absexample.py'))
