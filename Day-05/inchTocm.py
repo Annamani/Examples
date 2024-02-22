40302a6d6bb892672b51ebd6458c527b57ad154f
annamani <annamanik.vit@gmail.com>
2024-02-22 19:38:04 +0100

    examples

--- Day-05/inchTocm.py
+++ Day-05/inchTocm.py
@@ -2,8 +2,6 @@ print("Input your height: ")
 h_ft = int(input("Feet: "))
 h_inch = int(input("Inches: "))
 h_inch += h_ft * 12
-
-# Calculate the height in centimeters by multiplying by the conversion factor (2.54).
 h_cm = round(h_inch * 2.54, 1)
 
 # Print the calculated height in centimeters.
