1ea1b3b8cee181983a882c55d1b7b757acaba278
annamani <annamanik.vit@gmail.com>
2024-02-23 14:43:15 +0100

    examples

---
+++ Day-05/rightangleTriangle.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+print("Enter the Sides of a right angle triange: ")
+a = float(input("a: "))
+b = float(input("b: "))
+c=sqrt(a**2+b**2)
+print("The length of hypotenus: ",c)
