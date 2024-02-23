226da9a6ec3d31f1e6378821fd244f2c9d642b98
annamani <annamanik.vit@gmail.com>
2024-02-23 15:07:20 +0100

    examples

--- Day-05/distanceConverstion.py
+++ Day-05/distanceConverstion.py
@@ -5,9 +5,9 @@ d_ft = int(input("Input distance in feet: "))
 # 1 mile=1760 yards
 # 1 mile=5280 feet
 d_inches=d_ft*12
-d_yard=d_inches*36
-d_mile=1760*d_yard
-d_miles=5280*d_ft
+d_yard=d_inches/36
+d_mile=d_yard/1760
+d_miles=d_ft/5280
 print("Distance in Inches: ",d_inches)
 print("Distance in Yards: ",d_yard)
 print("Distance in Mile using yards: ",d_mile)
