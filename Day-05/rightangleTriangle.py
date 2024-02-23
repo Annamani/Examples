0af081a9a41fc0e195ba79b900ee23a0c80bf0ac
annamani <annamanik.vit@gmail.com>
2024-02-23 14:44:00 +0100

    examples

--- Day-05/rightangleTriangle.py
+++ Day-05/rightangleTriangle.py
@@ -1,6 +1,6 @@
 from math import sqrt
 
-print("Enter the Sides of a right angle triange: ")
+print("Enter the Sides of a right angle triangle: ")
 a = float(input("a: "))
 b = float(input("b: "))
 c=sqrt(a**2+b**2)
