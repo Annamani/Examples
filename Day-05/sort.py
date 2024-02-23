38505a05b1d4ecdabc9f4b602046d124bde2e90a
annamani <annamanik.vit@gmail.com>
2024-02-23 16:38:23 +0100

    examples

--- Day-05/sort.py
+++ Day-05/sort.py
@@ -1,5 +1,14 @@
-a,b,c=8,3,7
-maxn=a
-minn=b
-middle=(a+b+c)-maxn-minn
-print("Sort: ",minn,middle,maxn)
+# a,b,c=8,3,7
+# maxn=a
+# minn=b
+# middle=(a+b+c)-maxn-minn
+# print("Sort: ",minn,middle,maxn)
+
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+max_number=max(a,b,c)
+min_number=min(a,b,c)
+middle_number=(a+b+c)-max_number-min_number
+print("After sorting of numbers: "min_number,middle_number,max_number)
