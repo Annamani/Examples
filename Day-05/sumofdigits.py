fd411d3d7070f0f6fd8cf19d7c1fd2fd2f43311a
annamani <annamanik.vit@gmail.com>
2024-02-23 16:31:40 +0100

    examples

--- Day-05/sumofdigits.py
+++ Day-05/sumofdigits.py
@@ -2,7 +2,7 @@ num=int(input("Enter a number: "))
 sum=0
 while num>0:
     units=num%10
-    sum=sum+units
+    sum=int(sum)+units
     num=int(num)/10
 
 print("Sum of numbers in an integer: ",int(sum))
