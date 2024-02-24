b6fbdb264098483d0faff42dce04ab095084799c
annamani <annamanik.vit@gmail.com>
2024-02-24 15:03:53 +0100

    examples

--- Day-05/stringconcat.py
+++ Day-05/stringconcat.py
@@ -1,5 +1,5 @@
 list_of_colors = ['Red', 'White', 'Black']  
 colors = '-'.join(list_of_colors)  # Join the list elements with '-' and store the result in the 'colors' variable
-print()  # Print a blank line for spacing
+print() 
 print("All Colors: " + colors)  # Print the concatenated colors with '-' between them
 print()  # Print another blank line for spacing
