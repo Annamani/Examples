c40706c47ebe49aabd02d7ef4befde712fe4df30
annamani <annamanik.vit@gmail.com>
2024-02-24 14:56:52 +0100

    examples

---
+++ Day-05/linemidpoint.py
@@ -0,0 +1,10 @@
+# Print a message to inform the user that the program will calculate the midpoint of a line.
+print('\nCalculate the midpoint of a line :')
+
+# Prompt the user to enter the x and y coordinates of the first endpoint of the line.
+x1 = float(input('The value of x (the first endpoint) '))
+y1 = float(input('The value of y (the first endpoint) '))
+
+# Prompt the user to enter the x and y coordinates of the second endpoint of the line.
+x2 = float(input('The value of x (the first endpoint) '))
+y2 = float(input('The value of y (the first endpoint) '))
