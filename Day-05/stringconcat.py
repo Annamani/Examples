f23cc0ed3e00eaf6bcdae3c89a8ada2241107169
annamani <annamanik.vit@gmail.com>
2024-02-24 15:04:38 +0100

    examples

--- Day-05/stringconcat.py
+++ Day-05/stringconcat.py
@@ -1,5 +1,5 @@
 list_of_colors = ['Red', 'White', 'Black']  
-colors = '-'.join(list_of_colors)  # Join the list elements with '-' and store the result in the 'colors' variable
+colors = '-'.join(list_of_colors)
 print() 
 print("All Colors: " + colors) 
 print()  
