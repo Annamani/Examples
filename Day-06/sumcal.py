64075f57df9eae09da2a60f60a53afcf3ce75287
annamani <annamanik.vit@gmail.com>
2024-02-26 16:32:08 +0100

    examples

--- Day-06/sumcal.py
+++ Day-06/sumcal.py
@@ -14,3 +14,15 @@ nums = {'a': 100, 'b': 200, 'c': 300, 'd': 120}
 print(type(nums))
 print("Sum of all items of the said container:", dict_sum(nums))
 
+# Set
+# Create a set 'nums' with a collection of numbers.
+nums = {7, 4, 9, 1, 3, 2}
+# Print a message indicating the original container and display the set 'nums'.
+print("The original container")
+print(nums)
+# Print the type of the 'nums' container.
+print(type(nums))
+# Calculate the sum of all items in the set 'nums' using the 'sum' function and store it in the 'sum_tuple' variable.
+sum_tuple = sum(nums)
+# Print the sum of all items in the set.
+print("Sum of all items of the said container:", str(sum_tuple))
