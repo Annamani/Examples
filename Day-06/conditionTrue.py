ab0de19f644fccf716fa84fa3f51640a5e177cb5
annamani <annamanik.vit@gmail.com>
2024-02-27 17:43:38 +0100

    examples

---
+++ Day-06/conditionTrue.py
@@ -0,0 +1,10 @@
+# Prompt the user to input a number and convert it to a floating-point value.
+n = float(input("Input a number: "))
+
+# Check if the value of n is equal to 1.
+# If the condition is True, print the message indicating the first day of the month.
+if (n == 1):
+   print("First day of a Month!")
+# If the condition is False, print an empty line.
+else:
+   print()
