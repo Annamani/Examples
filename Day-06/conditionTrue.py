e239fe287d63cb5beeafb7c5434179d66c39db6b
annamani <annamanik.vit@gmail.com>
2024-02-27 17:44:08 +0100

    examples

--- Day-06/conditionTrue.py
+++ Day-06/conditionTrue.py
@@ -1,7 +1,5 @@
 n = int(input("Input a number: "))
 
-# Check if the value of n is equal to 1.
-# If the condition is True, print the message indicating the first day of the month.
 if (n == 1):
    print("First day of a Month!")
 # If the condition is False, print an empty line.
