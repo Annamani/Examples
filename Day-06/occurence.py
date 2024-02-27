19c367147abbaec06b46bcda436d2290b99410b7
annamani <annamanik.vit@gmail.com>
2024-02-27 17:39:03 +0100

    examples

--- Day-06/occurence.py
+++ Day-06/occurence.py
@@ -1,8 +1,6 @@
 nums = (7, 4, 9, 1, 3, 2)
 print("The original container")
 print(nums)
-
-# Print the type of the 'nums' container.
 print(type(nums))
 
 # Calculate the sum of all items in the tuple 'nums' using the 'sum' function and store it in the 'sum_tuple' variable.
