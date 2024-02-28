f40204151deafd48b28157681bcfaf8729899f2a
annamani <annamanik.vit@gmail.com>
2024-02-28 19:12:25 +0100

    examples

--- Day-06/Stringcheck.py
+++ Day-06/Stringcheck.py
@@ -1,6 +1,5 @@
 text = input("Input a word or numbers: ")
 if text.isdigit():
-    # If the input contains only digits, print "The input value is numbers."
     print("The input value is numbers.")
 else:
     # If the input contains characters other than digits, print "The input value is string."
