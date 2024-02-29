3bb8c141c91f0c6fb79a06d22d26cf517b3bcbdc
annamani <annamanik.vit@gmail.com>
2024-02-29 16:40:18 +0100

    examples

--- Day-06/removeitem.py
+++ Day-06/removeitem.py
@@ -1,6 +1,4 @@
 color = ["Red", "Black", "Green", "White", "Orange"]
 print("Original list elements:",color)
 del color[0]
-print("After removing the first color: ")
-print(color)
-print()
+print("After removing the first color: ",color)
