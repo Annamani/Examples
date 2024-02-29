2072c7044284ad0beb1da69944c4ec65848ef7e8
annamani <annamanik.vit@gmail.com>
2024-02-29 16:38:08 +0100

    examples

---
+++ Day-06/removeitem.py
@@ -0,0 +1,13 @@
+# Create a list of color names
+color = ["Red", "Black", "Green", "White", "Orange"]
+
+# Print the original list elements
+print("Original list elements:")
+print(color)
+
+# Remove the first element (element at index 0) from the list
+del color[0]
+# Print the list after removing the first color
+print("After removing the first color: ")
+print(color)
+print()
