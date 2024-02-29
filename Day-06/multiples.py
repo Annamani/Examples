3053c1fb4bc133b1cb7a96f636b296f0472e3147
annamani <annamanik.vit@gmail.com>
2024-02-29 16:35:24 +0100

    examples

--- Day-06/multiples.py
+++ Day-06/multiples.py
@@ -4,6 +4,6 @@
 #            print(num)
 
 list= [45, 55, 60, 37, 100, 105, 220]
-# result = list(filter(lambda x: (x % 13 == 0), list))
-# print(result)
+result = list(filter(lambda x: (x % 13 == 0), list))
+print(result)
 # multiples(list)
