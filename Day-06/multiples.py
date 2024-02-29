a28573e3b083f6350677a9e5a5cbc0bb1470f040
annamani <annamanik.vit@gmail.com>
2024-02-29 16:37:05 +0100

    examples

--- Day-06/multiples.py
+++ Day-06/multiples.py
@@ -3,7 +3,7 @@
 #       if num % 15 == 0:
 #            print(num)
 
-list1= [45, 55, 60, 37, 100, 105, 220]
-result = list(filter(lambda x: (x % 13 == 0), list1))
-print(result)
-# multiples(list)
+num_list = [45, 55, 60, 37, 100, 105, 220]
+result = list(filter(lambda x: (x % 15 == 0), num_list))
+print("Numbers divisible by 15 are", result)
+# multiples(num_list)
