356d4604f0e1262036a828bdf49d4f485aba0d9b
annamani <annamanik.vit@gmail.com>
2024-02-29 16:38:54 +0100

    examples

--- Day-06/removeitem.py
+++ Day-06/removeitem.py
@@ -7,7 +7,6 @@ print(color)
 
 # Remove the first element (element at index 0) from the list
 del color[0]
-# Print the list after removing the first color
 print("After removing the first color: ")
 print(color)
 print()
