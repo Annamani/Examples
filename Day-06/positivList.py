e193af5bf60a9b826ebfab61483d74e8e6604efc
annamani <annamanik.vit@gmail.com>
2024-03-01 16:27:21 +0100

    examples

--- Day-06/positivList.py
+++ Day-06/positivList.py
@@ -1,6 +1,4 @@
 nums = [34, 1, 0, -23, 12, -88]
 print("Original numbers in the list: ", nums)
 new_nums = list(filter(lambda x: x > 0, nums))
-
-# Print a message along with the list of positive numbers from the "nums" list.
 print("Positive numbers in the said list: ", new_nums)
