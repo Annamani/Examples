8757517cc7fd21fc4844bcdc0312eb2b59148e5e
annamani <annamanik.vit@gmail.com>
2024-03-01 16:22:02 +0100

    examples

--- Day-06/checkNum.py
+++ Day-06/checkNum.py
@@ -3,7 +3,5 @@ try:
     a = int(input("Input a number: "))
     break
 except ValueError:
-        # If the input is not a valid integer, an exception (ValueError) is raised.
-        # In that case, print an error message and prompt the user to try again.
-        print("\nThis is not a number. Try again...")
+    print("\nThis is not a number. Try again...")
         print()
