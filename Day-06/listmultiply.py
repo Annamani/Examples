daf8870330326d335a758e01220f82acf6194ebb
annamani <annamanik.vit@gmail.com>
2024-03-02 10:38:04 +0100

    examples

--- Day-06/listmultiply.py
+++ Day-06/listmultiply.py
@@ -2,9 +2,6 @@ from functools import reduce
 nums = [10, 20, 30]
 print("Original list numbers:")
 print(nums)
-
-# Calculate the product of the numbers in the list 'nums' using the 'reduce' function
-# and a lambda function that multiplies two numbers.
 nums_product = reduce((lambda x, y: x * y), nums)
 
 # Print the product of the numbers, obtained without using a for loop.
