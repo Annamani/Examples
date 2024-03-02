bf04e6a0f7d7de0e7b68bd73ff38509cf37ddd6b
annamani <annamanik.vit@gmail.com>
2024-03-02 10:41:29 +0100

    examples

--- Day-06/listmultiply.py
+++ Day-06/listmultiply.py
@@ -8,7 +8,5 @@
 import math
 nums = [10, 20, 30]
 nums_product = math.prod(nums)
-
-# Print the product of the numbers, obtained without using a for loop.
 print("\nProduct of the said numbers (without using a for loop):", nums_product)
 
