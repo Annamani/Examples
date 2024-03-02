e63377f63942b6b4350f156c5c906bb6cff20173
annamani <annamanik.vit@gmail.com>
2024-03-02 10:38:57 +0100

    examples

--- Day-06/listmultiply.py
+++ Day-06/listmultiply.py
@@ -4,3 +4,21 @@ print("Original list numbers:")
 print(nums)
 nums_product = reduce((lambda x, y: x * y), nums)
 print("\nProduct of the said numbers (without using a for loop):", nums_product)
+
+# Import the 'math' module to access the 'prod' function.
+import math
+
+# Define a list named 'nums' containing a series of integers.
+nums = [10, 20, 30]
+
+# Print a message indicating the original list of numbers.
+print("Original list numbers:")
+print(nums)
+
+# Calculate the product of the numbers in the list 'nums' using the 'prod' function
+# from the 'math' module.
+nums_product = math.prod(nums)
+
+# Print the product of the numbers, obtained without using a for loop.
+print("\nProduct of the said numbers (without using a for loop):", nums_product)
+
