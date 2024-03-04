dbd6edbd643c17082e73d6d9535460df46b2b2f7
annamani <annamanik.vit@gmail.com>
2024-03-04 16:51:31 +0100

    examples

--- Day-06/roundOfnum.py
+++ Day-06/roundOfnum.py
@@ -1,4 +1,3 @@
-# Define the order amount as a floating-point number.
 order_amt = 212.374
 print('\nThe total order amount comes to %f' % order_amt)
 print('The total order amount comes to %.2f' % order_amt)
