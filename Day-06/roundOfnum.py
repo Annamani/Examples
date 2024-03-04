65b8c0d52978949d5230f9c5ea05830a323892c1
annamani <annamanik.vit@gmail.com>
2024-03-04 16:52:34 +0100

    examples

--- Day-06/roundOfnum.py
+++ Day-06/roundOfnum.py
@@ -1,4 +1,5 @@
-input_value = 212.374
+# input_value = 212.374
+input_value=float(input("Enter some value in float: "))
 print('\nThe total order amount comes to %f' % input_value)
 print('The total order amount comes to %.2f' % input_value)
 
