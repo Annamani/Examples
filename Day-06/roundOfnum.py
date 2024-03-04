8089d11c7690e57c37b1e287182d74a5019a5e3e
annamani <annamanik.vit@gmail.com>
2024-03-04 16:51:10 +0100

    examples

--- Day-06/roundOfnum.py
+++ Day-06/roundOfnum.py
@@ -3,7 +3,5 @@ order_amt = 212.374
 
 # Print the total order amount with 6 decimal places.
 print('\nThe total order amount comes to %f' % order_amt)
-
-# Print the total order amount with 2 decimal places using string formatting.
 print('The total order amount comes to %.2f' % order_amt)
 
