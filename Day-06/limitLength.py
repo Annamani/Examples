a2221253fbf30f7fd2e308cb13b9f637bc6581de
annamani <annamanik.vit@gmail.com>
2024-03-04 16:54:33 +0100

    examples

--- Day-06/limitLength.py
+++ Day-06/limitLength.py
@@ -2,6 +2,4 @@ str_num = "1234567890"
 print("Original string:", str_num)
 print('%.6s' % str_num)
 print('%.9s' % str_num)
-
-# Print the entire string (first 10 characters) as it is.
 print('%.10s' % str_num)
