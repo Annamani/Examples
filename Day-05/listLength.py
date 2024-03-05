350fd2ca6dd11c996f63fe3cdb422a49bbe9a125
annamani <annamanik.vit@gmail.com>
2024-03-05 17:19:29 +0100

    examples

---
+++ Day-05/listLength.py
@@ -0,0 +1,5 @@
+# Create a list 'nums' containing various integer values.
+nums = [2, 2, 4, 6, 6, 8, 6, 10, 4]
+
+# Calculate and print the length (number of elements) of the 'nums' list.
+print(len(nums))
