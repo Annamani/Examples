c6f199b14296b5a3673a22af7b7e10042acfb58b
annamani <annamanik.vit@gmail.com>
2024-03-05 17:17:47 +0100

    examples

--- Day-05/multiplevariables.py
+++ Day-05/multiplevariables.py
@@ -3,5 +3,6 @@
 # z = 20
 x=int(input("Enter value for x: "))
 y=int(input("Enter value for y: "))
+z=int(input("Enter value for z: "))
 if x == y == z == 20:
     print("All variables have the same value!")
