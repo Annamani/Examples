475c6ceb849e11130f27dd198d43f273474b9a39
annamani <annamanik.vit@gmail.com>
2024-03-05 17:16:09 +0100

    examples

--- Day-05/multiplevariables.py
+++ Day-05/multiplevariables.py
@@ -1,11 +1,6 @@
 x = 20
-
-# Assign the value 20 to the variable y.
 y = 20
-
-# Assign the value 20 to the variable z.
 z = 20
 
-# Check if all variables have the value 20 using the equality (==) operator.
 if x == y == z == 20:
     print("All variables have the same value!")
