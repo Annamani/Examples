07555d6e297711add31429107abe4360fad3b0ed
annamani <annamanik.vit@gmail.com>
2024-03-06 17:23:07 +0100

    examples

---
+++ Day-05/checkInteger.py
@@ -0,0 +1,15 @@
+# Define a function 'test' that takes a parameter 'n'.
+def test(n):
+    # Check if the type of 'n' is equal to 'int'.
+    if type(n) == int:
+         # If 'n' is an integer, return the message "It is an integer!"
+         return "It is an integer!"
+    else:
+         # If 'n' is not an integer, return the message "It is a String!"
+         return "It is a String!"     
+
+# Call the 'test' function with an integer argument (12) and print the result.
+print(test(12))
+
+# Call the 'test' function with a string argument ('23312') and print the result.
+print(test('23312'))
