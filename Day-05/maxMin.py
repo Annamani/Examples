a0eed8fdbbb01cf0404003be69886e20bf77f493
annamani <annamanik.vit@gmail.com>
2024-03-07 18:19:34 +0100

    examples

--- Day-05/maxMin.py
+++ Day-05/maxMin.py
@@ -2,16 +2,12 @@ def max_min(data):
     max_value = data[0] 
     min_value = data[0]  
     
-    # Iterate through each number 'num' in the 'data' list.
     for num in data:
-        # Check if the current number 'num' is greater than the current maximum 'l'.
         if num > max_value:
-            max_value = num  # If 'num' is greater, update 'l' with 'num'.
-        # Check if the current number 'num' is smaller than the current minimum 's'.
+            max_value = num  
         elif num < min_value:
-            min_value = num  # If 'num' is smaller, update 's' with 'num'.
+            min_value = num  
     
-    # Return the maximum 'l' and minimum 's' values as a tuple.
     return max_value, min_value
 
 # Call the 'max_min' function with a list of numbers and print the result.
