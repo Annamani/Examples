3e3369de2faddaaf131aa4a7c08bdd6c9624d1e0
annamani <annamanik.vit@gmail.com>
2024-03-07 18:14:20 +0100

    examples

--- Day-05/checkDivision.py
+++ Day-05/checkDivision.py
@@ -1,8 +1,5 @@
 # Define a function named 'multiple' that takes two arguments, 'm' and 'n'.
 def multiple(m, n):
-    # Check if 'm' is a multiple of 'n' by using the modulo operator.
-    # If the remainder is 0, return True, indicating 'm' is a multiple of 'n'.
-    # Otherwise, return False.
     return True if m % n == 0 else False
 
 print(multiple(20, 5))  
