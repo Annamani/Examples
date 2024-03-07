b678a59e69a701310c17f805c88fb329ac971737
annamani <annamanik.vit@gmail.com>
2024-03-07 18:13:27 +0100

    examples

--- Day-05/checkDivision.py
+++ Day-05/checkDivision.py
@@ -6,5 +6,5 @@ def multiple(m, n):
     return True if m % n == 0 else False
 
 # Call the 'multiple' function with different arguments and print the results.
-print(multiple(20, 5))  # Check if 20 is a multiple of 5 and print the result.
+print(multiple(20, 5))  
 print(multiple(7, 2))   # Check if 7 is a multiple of 2 and print the result.
