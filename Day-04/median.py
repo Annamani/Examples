52c573708f75f3899c44bd4e813c3749a4e38b52
annamani <annamanik.vit@gmail.com>
2024-03-08 19:49:24 +0100

    examples

---
+++ Day-04/median.py
@@ -0,0 +1,25 @@
+# Input the first number from the user
+x = input("Input the first number: ")
+
+# Input the second number from the user
+y = input("Input the second number: ")
+
+# Input the third number from the user
+z = input("Input the third number: ")
+
+# Print a message indicating that the program will calculate the median
+print("Median of the above three numbers -")
+
+# Check conditions to determine the median and print the result
+if y < x and x < z:
+    print(x)
+elif z < x and x < y:
+    print(x)
+elif z < y and y < x:
+    print(y)
+elif x < y and y < z:
+    print(y)
+elif y < z and z < x:
+    print(z)    
+elif x < z and z < y:
+    print(z)
