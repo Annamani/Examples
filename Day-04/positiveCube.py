7a2dd7ed01c86c31408271e36bc356fa7299edb5
annamani <annamanik.vit@gmail.com>
2024-03-08 19:30:27 +0100

    examples

--- Day-04/positiveCube.py
+++ Day-04/positiveCube.py
@@ -2,6 +2,8 @@ def cubeOfPositiveNumber(num):
     sum=0
     number=n-1
     while(number>0):
+        sum+=number*number*number
+    return sum
 
 num=int(input("Enter a positive number: "))
 print(cubeOfPositiveNumber(num))
