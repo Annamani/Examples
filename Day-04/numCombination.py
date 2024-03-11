3b4990d980fb6c9cff8901782a3a2d49c6c1b920
annamani <annamanik.vit@gmail.com>
2024-03-11 15:56:30 +0100

    examples

--- Day-04/numCombination.py
+++ Day-04/numCombination.py
@@ -5,7 +5,5 @@ numbers = []
 for num in range(1000):
     # Convert the number to a string and fill with zeros to make it three digits long.
     num = str(num).zfill(3)
-
-# Print the last formatted number (outside the loop, indentation issue).
 print(num)
 numbers.append(num)
