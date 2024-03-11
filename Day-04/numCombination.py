f3d8ad7f0c97b0f7599c45f57a716c584b8fc108
annamani <annamanik.vit@gmail.com>
2024-03-11 15:56:44 +0100

    examples

--- Day-04/numCombination.py
+++ Day-04/numCombination.py
@@ -1,7 +1,6 @@
 # Create an empty list to store formatted numbers.
 numbers = []
 
-# Iterate through numbers from 0 to 999 (exclusive).
 for num in range(1000):
     # Convert the number to a string and fill with zeros to make it three digits long.
     num = str(num).zfill(3)
