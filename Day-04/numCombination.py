0dec8318b464e781560e02820f9ae3f3b94147d4
annamani <annamanik.vit@gmail.com>
2024-03-11 15:56:53 +0100

    examples

--- Day-04/numCombination.py
+++ Day-04/numCombination.py
@@ -2,7 +2,6 @@
 numbers = []
 
 for num in range(1000):
-    # Convert the number to a string and fill with zeros to make it three digits long.
     num = str(num).zfill(3)
 print(num)
 numbers.append(num)
