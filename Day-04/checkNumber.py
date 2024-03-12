057dd9a75eef961cb16c23257072377a81443de5
annamani <annamanik.vit@gmail.com>
2024-03-12 17:51:35 +0100

    examples

--- Day-04/checkNumber.py
+++ Day-04/checkNumber.py
@@ -2,7 +2,6 @@
 
 # Define a function named 'test' that takes a list 'nums' as input
 def test(nums):
-    # Check if the count of 19 in 'nums' is equal to 2 and the count of 5 is greater than or equal to 3
     return nums.count(19) == 2 and nums.count(5) >= 3
 
 nums = [19, 19, 15, 5, 3, 5, 5, 2]
