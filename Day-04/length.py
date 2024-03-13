dec75fd3fbba0a28ec743e9300554833410daf7c
annamani <annamanik.vit@gmail.com>
2024-03-13 17:38:17 +0100

    examples

--- Day-04/length.py
+++ Day-04/length.py
@@ -2,9 +2,7 @@ def test(strs):
     # Use the map function to apply the len function to each string in 'strs', and convert the result to a list
     return [*map(len, strs)]
 strs = ['cat', 'car', 'fear', 'center']
-print("Original strings:")
 print(strs)
-print("Lengths of the said list of non-empty strings:")
 print(test(strs))
 strs = ['cat', 'dog', 'shatter', 'donut', 'at', 'todo', '']
 print(strs)
