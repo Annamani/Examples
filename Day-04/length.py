ec43e19ed9d875188cf6a06e6cfb121dfdc8a2ef
annamani <annamanik.vit@gmail.com>
2024-03-13 17:35:07 +0100

    examples

---
+++ Day-04/length.py
@@ -0,0 +1,32 @@
+# License: https://bit.ly/3oLErEI
+
+# Define a function named 'test' that takes a list of strings 'strs' as input
+def test(strs):
+    # Use the map function to apply the len function to each string in 'strs', and convert the result to a list
+    return [*map(len, strs)]
+
+# Create a list of strings 'strs' with specific elements
+strs = ['cat', 'car', 'fear', 'center']
+
+# Print the original list of strings
+print("Original strings:")
+print(strs)
+
+# Print a message indicating the operation to be performed on the list
+print("Lengths of the said list of non-empty strings:")
+
+# Print the result of the test function applied to the 'strs' list
+print(test(strs))
+
+# Create a different list of strings 'strs' with specific elements
+strs = ['cat', 'dog', 'shatter', 'donut', 'at', 'todo', '']
+
+# Print the original list of strings
+print("\nOriginal strings:")
+print(strs)
+
+# Print a message indicating the operation to be performed on the list
+print("Lengths of the said list of non-empty strings:")
+
+# Print the result of the test function applied to the modified 'strs' list
+print(test(strs))
