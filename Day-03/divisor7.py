25ad81194b0368cc2ed9154248173d42cecdbf10
annamani <annamanik.vit@gmail.com>
2024-03-14 17:49:08 +0100

    examples

--- Day-03/divisor7.py
+++ Day-03/divisor7.py
@@ -1,6 +1,5 @@
 nl = []
 for x in range(1500, 2701):
-    # Check if the number is divisible by 7 and 5 without any remainder
     if (x % 7 == 0) and (x % 5 == 0):
         # If the conditions are met, convert the number to a string and append it to the list
         nl.append(str(x))
