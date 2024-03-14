ac02f19c18a64e463c01c1b36aefaa855a92b319
annamani <annamanik.vit@gmail.com>
2024-03-14 17:48:51 +0100

    examples

--- Day-03/divisor7.py
+++ Day-03/divisor7.py
@@ -1,6 +1,4 @@
 nl = []
-
-# Iterate through numbers from 1500 to 2700 (inclusive)
 for x in range(1500, 2701):
     # Check if the number is divisible by 7 and 5 without any remainder
     if (x % 7 == 0) and (x % 5 == 0):
