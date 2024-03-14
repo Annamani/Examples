3198413e5238e0a171451a9571291349ce528e8a
annamani <annamanik.vit@gmail.com>
2024-03-14 17:52:49 +0100

    examples

--- Day-03/guessNum.py
+++ Day-03/guessNum.py
@@ -1,7 +1,6 @@
 import random
 target_num, guess_num = random.randint(1, 10), 0
 while target_num != guess_num:
-    # Prompt the user to input a number between 1 and 10 and convert it to an integer
     guess_num = int(input('Guess a number between 1 and 10 until you get it right : '))
 
 # Print a message indicating successful guessing once the correct number is guessed
