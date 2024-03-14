388d0c22338fc7a768df7a5efe7a262a4d422222
annamani <annamanik.vit@gmail.com>
2024-03-14 17:53:32 +0100

    examples

--- Day-03/guessNum.py
+++ Day-03/guessNum.py
@@ -3,5 +3,4 @@ target_num, guess_num = random.randint(1, 10), 0
 while target_num != guess_num:
     guess_num = int(input('Guess a number between 1 and 10 until you get it right : '))
 
-# Print a message indicating successful guessing once the correct number is guessed
 print('Well guessed!') 
