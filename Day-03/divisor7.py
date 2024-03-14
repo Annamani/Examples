fc5513b40eaa57ffd073ceb26165078b7b3d04e0
annamani <annamanik.vit@gmail.com>
2024-03-14 17:49:28 +0100

    examples

--- Day-03/divisor7.py
+++ Day-03/divisor7.py
@@ -1,7 +1,6 @@
 nl = []
 for x in range(1500, 2701):
     if (x % 7 == 0) and (x % 5 == 0):
-        # If the conditions are met, convert the number to a string and append it to the list
         nl.append(str(x))
 
 # Join the numbers in the list with a comma and print the result
