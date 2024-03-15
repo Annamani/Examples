5e7fbe5e64e9d456a9ce9d22a0723573fb266525
annamani <annamanik.vit@gmail.com>
2024-03-15 14:33:33 +0100

    examples

--- Day-03/ListSum.py
+++ Day-03/ListSum.py
@@ -4,5 +4,10 @@ def sum(numbers):
         total += x
     return total
 
-# Print the result of calling the 'sum' function with a tuple of numbers (8, 2, 3, 0, 7)
+def multiply(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+
 print(sum((8, 2, 3, 0, 7))) 
