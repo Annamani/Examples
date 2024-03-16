ed14065934d9896b2de4644c4425aea568d8932d
annamani <annamanik.vit@gmail.com>
2024-03-16 11:41:05 +0100

    examples

--- Day-03/stringTest.py
+++ Day-03/stringTest.py
@@ -1,20 +1,11 @@
-# Define a function named 'string_test' that counts the number of upper and lower case characters in a string 's'
 def string_test(s):
-    # Create a dictionary 'd' to store the count of upper and lower case characters
     d = {"UPPER_CASE": 0, "LOWER_CASE": 0}
-    
-    # Iterate through each character 'c' in the string 's'
     for c in s:
-        # Check if the character 'c' is in upper case
         if c.isupper():
-            # If 'c' is upper case, increment the count of upper case characters in the dictionary
             d["UPPER_CASE"] += 1
-        # Check if the character 'c' is in lower case
         elif c.islower():
-            # If 'c' is lower case, increment the count of lower case characters in the dictionary
             d["LOWER_CASE"] += 1
         else:
-            # If 'c' is neither upper nor lower case (e.g., punctuation, spaces), do nothing
             pass
     
     print("Original String: ", s)
