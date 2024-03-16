8cbf646d9d0722fb13b09e65cf474109196fff75
annamani <annamanik.vit@gmail.com>
2024-03-16 11:37:47 +0100

    examples

--- Day-03/test_range.py
+++ Day-03/test_range.py
@@ -1,2 +1,9 @@
+def test_range(num):
+    if num in range(0,11):
+        print(num," is in the Given range")
+    else:
+        print(num," is NOT in the Given range")
+
+
 num=int(input("Enter a number: "))
-print(test_range(num))
+test_range(num)
