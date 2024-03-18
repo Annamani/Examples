c3bcc80c8ede21b386e89d63e506819eea1f713b
annamani <annamanik.vit@gmail.com>
2024-03-18 15:12:34 +0100

    examples

--- Day-03/evenList.py
+++ Day-03/evenList.py
@@ -1,6 +1,5 @@
 # Define a function named 'is_even_num' that takes a list 'l' as input and returns a list of even numbers
 def is_even_num(l):
-    # Create an empty list 'enum' to store even numbers
     enum = []
     for n in l:
         if n % 2 == 0:
