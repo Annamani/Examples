5081dfe61cea86f8c3eab30e71d55c645881d294
annamani <annamanik.vit@gmail.com>
2024-03-19 17:06:14 +0100

    examples

--- Day-03/uniqueList.py
+++ Day-03/uniqueList.py
@@ -1,6 +1,5 @@
 # Define a function named 'unique_list' that takes a list 'l' as input and returns a list of unique elements
 def unique_list(l):
-    # Create an empty list 'x' to store unique elements
     x = []
     for a in l:
         if a not in x:
