7713764e0cc08b0ac30ec7cbb4b8d865ee20521b
annamani <annamanik.vit@gmail.com>
2024-03-20 20:38:22 +0100

    examples

--- Day-03/perfectNum.py
+++ Day-03/perfectNum.py
@@ -1,6 +1,5 @@
 # Define a function named 'perfect_number' that checks if a number 'n' is a perfect number
 def perfect_number(n):
-    # Initialize a variable 'sum' to store the sum of factors of 'n'
     sum = 0
     for x in range(1, n):
         if n % x == 0:
