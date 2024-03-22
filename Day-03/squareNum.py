6e2111a8389f3392bb180e3a69f2da97471e432d
annamani <annamanik.vit@gmail.com>
2024-03-22 16:31:01 +0100

    examples

--- Day-03/squareNum.py
+++ Day-03/squareNum.py
@@ -1,6 +1,5 @@
 # Define a function named 'printValues' that generates a list of squares of numbers from 1 to 20
 def printValues():
-    # Create an empty list 'l'
     l = list()
     for i in range(1, 21):
         l.append(i**2)
