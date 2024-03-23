94ee6ef143f7230bb9b00e425a39881898904698
annamani <annamanik.vit@gmail.com>
2024-03-23 11:15:32 +0100

    examples

--- Day-03/dataToArray.py
+++ Day-03/dataToArray.py
@@ -2,7 +2,7 @@ def file_read(fname):
     content_array = []
     with open(fname) as f:
         for line in f:
-                    content_array.append(line)
+            content_array.append(line)
                 print(content_array)
 
 file_read('test.txt')
