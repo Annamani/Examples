306385a72a749c78d52cd969acb3a3e29da9e149
annamani <annamanik.vit@gmail.com>
2024-03-25 14:08:07 +0100

    examples

--- Day-01/DictUpdate.py
+++ Day-01/DictUpdate.py
@@ -1,7 +1,5 @@
 d = {0: 10, 1: 20}
 print(d)
-
-# Update the dictionary 'd' by adding a new key-value pair {2: 30}.
 d.update({2: 30})
 
 # Print the dictionary 'd' after the update, which now includes the new key-value pair.
