52cd8a53e591d46c95afebb1fc9c29be5ba23558
annamani <annamanik.vit@gmail.com>
2024-03-25 14:10:12 +0100

    examples

--- Day-01/DictUpdate.py
+++ Day-01/DictUpdate.py
@@ -1,4 +1,12 @@
-d = {0: 10, 1: 20}
-print(d)
-d.update({2: 30})
-print(d) 
+# d = {0: 10, 1: 20}
+# print(d)
+# d.update({2: 30})
+# print(d) 
+
+
+dict_var={"color1":"Green",
+          "color2":"Blue",
+          "color3":"Red",
+          "color4":"Black",
+          "color5":"Gray",
+          "color6":"Violet"}
