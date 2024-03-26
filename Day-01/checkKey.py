bea30e0accb4547b8d7268285ed6b5b6db4e6427
annamani <annamanik.vit@gmail.com>
2024-03-26 13:40:32 +0100

    examples

--- Day-01/checkKey.py
+++ Day-01/checkKey.py
@@ -1,1 +1,7 @@
-d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+def checkKey(key):
+    if key in dict_values:
+
+
+dict_values = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+key=int(input("Enter a key value to check in dictionary: "))
+checkKey(key)
