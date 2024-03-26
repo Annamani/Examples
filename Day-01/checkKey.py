5be0d9c94e742e6b2384ad17ab8f736e8a9c46f4
annamani <annamanik.vit@gmail.com>
2024-03-26 13:41:50 +0100

    examples

--- Day-01/checkKey.py
+++ Day-01/checkKey.py
@@ -1,8 +1,8 @@
 def checkKey(key):
     if key in dict_values:
-        print("Key is present in dictionary values")
+        print("Key is present in dictionary values\n")
     else:
-        print("Key is not present in dictionary values")
+        print("Key is not present in dictionary values\n")
 
 dict_values = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 key=int(input("Enter a key value to check in dictionary: "))
