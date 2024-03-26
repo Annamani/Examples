7eb3c7baabeb4ae4e0d7d9d75667453041f7a066
annamani <annamanik.vit@gmail.com>
2024-03-26 13:47:52 +0100

    examples

--- Day-01/iterateDict.py
+++ Day-01/iterateDict.py
@@ -1,4 +1,3 @@
 dict_values = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 for key,value in dict_values.items():
-    print("Dictionary values are: ")
-    
+    print(f"Dictionary values are: {key} --> {value}")
