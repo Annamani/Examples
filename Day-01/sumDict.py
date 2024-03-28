0ff2150171776dd0053dc04a5624db63876aaa46
annamani <annamanik.vit@gmail.com>
2024-03-28 19:43:24 +0100

    examples

--- Day-01/sumDict.py
+++ Day-01/sumDict.py
@@ -2,6 +2,6 @@ my_dict = {'data1': 100, 'data2': -54, 'data3': 247}
 # result = sum(my_dict.values())
 # print(result)
 sum=0
-for value in my_dict.items():
-    sum+=my_dict[value]
+for key,value in my_dict.values():
+    sum=sum+my_dict[value]
 print(sum)
