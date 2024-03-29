3082ad0780d045386aa28756d8c9bcc3d5e50e99
annamani <annamanik.vit@gmail.com>
2024-03-29 09:47:08 +0100

    examples

--- Day-01/DictionaryEx/removeKey.py
+++ Day-01/DictionaryEx/removeKey.py
@@ -2,6 +2,4 @@ myDict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 print("Original Dictionary: \n",myDict)
 if 'a' in myDict:
     del myDict['a']
-
-# Print the updated dictionary 'myDict' after deleting the key 'a' (if it existed).
 print(myDict)
