b2e998c18f1fb715b8ae660037bf4eb8b77f3f19
annamani <annamanik.vit@gmail.com>
2024-03-29 09:56:48 +0100

    examples

--- Day-01/DictionaryEx/listTodict.py
+++ Day-01/DictionaryEx/listTodict.py
@@ -1,4 +1,5 @@
 keys=['color1','color2','color3','color4','color5']
 values=['red','blue','white','green','black']
-color_dictionary = dict(zip(keys, values))
+# color_dictionary = dict(zip(keys, values))
+color_dictionary={keys[i]: values[i] for i in range(len(keys))}
 print(color_dictionary)
