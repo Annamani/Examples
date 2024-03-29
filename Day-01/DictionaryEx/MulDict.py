8680c0862fd77c4c31b3c690c27ee2af0f5d63ac
annamani <annamanik.vit@gmail.com>
2024-03-29 09:43:07 +0100

    examples

--- Day-01/DictionaryEx/MulDict.py
+++ Day-01/DictionaryEx/MulDict.py
@@ -1,7 +1,5 @@
 my_dict = {'data1': 100, 'data2': -54, 'data3': 247}
-result = mul(my_dict.values())
-print(result)
-# sum=0
-# for value in my_dict.values():
-#     sum=sum+value
-# print(sum)
+sum=0
+for value in my_dict.values():
+    sum=sum+value
+print(sum)
